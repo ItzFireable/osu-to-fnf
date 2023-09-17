c159f5ca30e6f2efc766e57d22235335c0dd81ca
ItzFireable <fireableplayz@gmail.com>
2023-09-18 01:15:08 +0300

    fixes, UI changes

--- osu/osu_to_lua.js
+++ osu/osu_to_lua.js
@@ -15,37 +15,7 @@ module.export("osu_to_lua", function(osu_file_contents, options) {
 	}
 
 	function hitobj_x_to_track_number(hitobj_x) {
-		var track_number = 0;
-		if (options.onlyp1 === false) {
-			if (hitobj_x <= 32) {
-				track_number = 0;
-			} else if (hitobj_x <= 96) {
-				track_number = 1;
-			} else if (hitobj_x <= 160) {
-				track_number = 2;
-			} else if (hitobj_x <= 224) {
-				track_number = 3;
-			} else if (hitobj_x <= 288) {
-				track_number = 4;
-			} else if (hitobj_x <= 352) {
-				track_number = 5;
-			} else if (hitobj_x <= 416) {
-				track_number = 6;
-			} else {
-				track_number = 7;
-			}
-		} else {
-			if (hitobj_x < 100) {
-				track_number = 0;
-			} else if (hitobj_x < 200) {
-				track_number = 1;
-			} else if (hitobj_x < 360) {
-				track_number = 2;
-			} else {
-				track_number = 3;
-			}
-		}
-		return track_number;
+		return Math.floor(hitobj_x * options.keycount / 512);
 	}
 
 	function msToTime(s) {
@@ -122,9 +92,9 @@ module.export("osu_to_lua", function(osu_file_contents, options) {
 		}
 	}
 	if (options.bpm === "") {
-		options.bpm = (1 / beatmap.timingPoints[0].beatLength * 1000 * 60).toFixed(6);
+		options.bpm = beatmap.timingPoints[0].beatLength > 0 ? (1 / beatmap.timingPoints[0].beatLength * 1000 * 60) : (1 / beatmap.timingPoints[1].beatLength * 1000 * 60);
 	}
-	options.bpm = parseInt(options.bpm);
+	options.bpm = parseFloat(options.bpm);
 	if (isNaN(options.bpm)) {
 		options.bpm = 0
 	}
@@ -142,6 +112,15 @@ module.export("osu_to_lua", function(osu_file_contents, options) {
 		options.speed = 1;
 	}
 
+	if (options.keycount === ""){
+		options.keycount = 4;
+	}
+	options.keycount = parseInt(options.keycount);
+	if (isNaN(options.keycount)) {
+		options.keycount = 4;
+	}
+
+	options.lanes = options.convert == true ? 1 : 2;
 	if (options.bpm == 0)
 		return 'Error: No BPM found.';
 
@@ -154,6 +133,7 @@ module.export("osu_to_lua", function(osu_file_contents, options) {
 	var prevOffset = 0;
 	
 	var baseBPM = 0;
+	console.log(options.bpm)
 
 	if (options.svs === true) {
 		append_to_output(`{"sliderVelocities":[{"multiplier": 1, "startTime": 0},`)
@@ -183,67 +163,112 @@ module.export("osu_to_lua", function(osu_file_contents, options) {
 		}
 
 		rtv_lua = rtv_lua.slice(0, -1)
-		append_to_output(`],"song":{"player2":"${options.player2}","player1":"${options.player1}","speed":${options.speed},"needsVoices":${options.needsvoices},"sectionLengths":[],"song":"${options.song}","bpm":${options.bpm},"sections":0,"validScore":true,"notes":[`)
+		append_to_output(`],"song":{"player2":"${options.player2}","player1":"${options.player1}","speed":${options.speed},"needsVoices":false,"sectionLengths":[],"song":"${options.song}","bpm":${options.bpm},"sections":0,"validScore":true,"notes":[`)
 	} else {
-		append_to_output(`{"song":{"player2":"${options.player2}","player1":"${options.player1}","speed":${options.speed},"needsVoices":${options.needsvoices},"sectionLengths":[],"song":"${options.song}","bpm":${options.bpm},"sections":0,"validScore":true,"notes":[`)
+		append_to_output(`{"song":{"player2":"${options.player2}","player1":"${options.player1}","speed":${options.speed},"needsVoices":false,"sectionLengths":[],"song":"${options.song}","bpm":${options.bpm},"sections":0,"validScore":true,"notes":[`)
 	}
 
-	if (options.onlyp1 === false){
-		while (true) {
-			var p1 = false;
-			var p2 = false;
-			
-			var notes = [];
-			for (var i = 0; i < beatmap.hitObjects.length; i++) {
-				if (beatmap.hitObjects[i].startTime >= currentlength) {
-					if (beatmap.hitObjects[i].startTime < currentlength + sectionlength) {
-						if (hitobj_x_to_track_number(beatmap.hitObjects[i].position[0]) <= 3) {
-							p2 = true;
-						} else {
-							p1 = true;
-						}
-						notes.push(beatmap.hitObjects[i])
+	while (true) {
+		var p1 = false;
+		var p2 = false;
+		
+		var notes = [];
+		for (var i = 0; i < beatmap.hitObjects.length; i++) {
+			if (beatmap.hitObjects[i].startTime >= currentlength) {
+				if (beatmap.hitObjects[i].startTime < currentlength + sectionlength) {
+					if (hitobj_x_to_track_number(beatmap.hitObjects[i].position[0]) <= (options.keycount / options.lanes) - 1) {
+						p2 = true;
 					} else {
-						break
+						p1 = true;
+					}
+					notes.push(beatmap.hitObjects[i])
+				} else {
+					break
+				}
+			}
+		}
+		var camera = false;
+		if (p2 === false && p1 === true) {
+			camera = true;
+		} else if (p2 === true && p1 === false) {
+			camera = false;
+		} else if (p2 === true && p1 === true) {
+			camera = true;
+		} else if (p2 === false && p1 === false) {
+			camera = true;
+		}
+
+		if (options.onlyp1 === true)
+			camera = false;
+
+		append_to_output(`{"mustHitSection":${camera},"typeOfSection":0,"lengthInSteps":16,"sectionNotes":[`)
+
+		for (var i = 0; i < notes.length; i++) {
+			append_to_output(`[${notes[i].startTime},`)
+
+			var track = hitobj_x_to_track_number(notes[i].position[0]);
+
+			if (camera === false) {
+				append_to_output(track)
+			} else if (camera === true && p1 === true && p2 === false) {
+				append_to_output(track - (options.keycount / options.lanes))
+			} else if (camera === true && p1 === true && p2 === true) {
+				if (track > (options.keycount / options.lanes) - 1) {
+					append_to_output(track - (options.keycount / options.lanes))
+				} else if (track < (options.keycount / options.lanes)) {
+					append_to_output(track + (options.keycount / options.lanes))
+				}
+
+			} else if (camera === true && p2 === false && p1 === false) {
+				if (track > (options.keycount / options.lanes) - 1) {
+					append_to_output(track - (options.keycount / options.lanes))
+				} else if (track < (options.keycount / options.lanes)) {
+					append_to_output(track + (options.keycount / options.lanes))
+				}
+			}
+
+			if (notes[i].duration === undefined) {
+				append_to_output(`,0]`)
+			} else {
+				append_to_output(`,${notes[i].duration}]`)
+			}
+
+			if (options.twoplayers === true){
+				if (notes[i].soundTypes.includes("whistle") || notes[i].soundTypes.includes("clap")) {
+					rtv_lua = rtv_lua.slice(0, -1)
+					
+					if (notes[i].soundTypes.includes("whistle")) {
+						append_to_output(`,"Alt Animation"]`)
+					} else if (notes[i].soundTypes.includes("clap")) {
+						append_to_output(`,"GF Sing"]`)
 					}
 				}
 			}
-			var camera = true;
-			if (p2 === false && p1 === true) {
-				camera = true;
-			} else if (p2 === true && p1 === false) {
-				camera = false;
-			} else if (p2 === true && p1 === true) {
-				camera = true;
-			} else if (p2 === false && p1 === false) {
-				camera = true;
-			}
 
-			append_to_output(`{"mustHitSection":${camera},"typeOfSection":0,"lengthInSteps":16,"sectionNotes":[`)
-	
-			for (var i = 0; i < notes.length; i++) {
-				append_to_output(`[${notes[i].startTime},`)
-				var track = hitobj_x_to_track_number(notes[i].position[0]);
-	
+			if (options.convert) {
+				append_to_output(`,[${notes[i].startTime},`)
+
+				var track = hitobj_x_to_track_number(notes[i].position[0]) + (options.keycount / options.lanes);
+
 				if (camera === false) {
 					append_to_output(track)
 				} else if (camera === true && p1 === true && p2 === false) {
-					append_to_output(track - 4)
+					append_to_output(track - (options.keycount / options.lanes))
 				} else if (camera === true && p1 === true && p2 === true) {
-					if (track > 3) {
-						append_to_output(track - 4)
-					} else if (track < 4) {
-						append_to_output(track + 4)
+					if (track > (options.keycount / options.lanes) - 1) {
+						append_to_output(track - (options.keycount / options.lanes))
+					} else if (track < (options.keycount / options.lanes)) {
+						append_to_output(track + (options.keycount / options.lanes))
 					}
 	
 				} else if (camera === true && p2 === false && p1 === false) {
-					if (track > 3) {
-						append_to_output(track - 4)
-					} else if (track < 4) {
-						append_to_output(track + 4)
+					if (track > (options.keycount / options.lanes) - 1) {
+						append_to_output(track - (options.keycount / options.lanes))
+					} else if (track < (options.keycount / options.lanes)) {
+						append_to_output(track + (options.keycount / options.lanes))
 					}
 				}
-	
+
 				if (notes[i].duration === undefined) {
 					append_to_output(`,0]`)
 				} else {
@@ -261,69 +286,31 @@ module.export("osu_to_lua", function(osu_file_contents, options) {
 						}
 					}
 				}
-				
-				if (i !== notes.length - 1) {
-					append_to_output(",")
-				}
 			}
-	
-			append_to_output(`]}`)
-			currentlength += sectionlength;
-	
-			var breaking = true;
-			for (var i = 0; i < beatmap.hitObjects.length; i++) {
-				if (beatmap.hitObjects[i].startTime >= currentlength) {
-					breaking = false;
-					break;
-				}
-			}
-			if (breaking === false) {
+			
+			if (i !== notes.length - 1) {
 				append_to_output(",")
-			} else {
+			}
+		}
+
+		append_to_output(`]}`)
+		currentlength += sectionlength;
+
+		var breaking = true;
+		for (var i = 0; i < beatmap.hitObjects.length; i++) {
+			if (beatmap.hitObjects[i].startTime >= currentlength) {
+				breaking = false;
 				break;
 			}
 		}
-	} else {
-		append_to_output(`{"mustHitSection":true,"typeOfSection":0,"lengthInSteps":16,"sectionNotes":[`)
-		for (var i = 0; i < beatmap.hitObjects.length; i++) {
-			var itr = beatmap.hitObjects[i];
-			var type = itr.objectName;
-			var track = hitobj_x_to_track_number(itr.position[0]);
-
-			append_to_output(`[${itr.startTime},${track}`)
-
-			if (itr.duration === undefined) {
-				append_to_output(`,0]`)
-			} else {
-				append_to_output(`,${itr.duration}]`)
-			}
-
-			if (options.convert === true) {
-				append_to_output(",")
-
-				append_to_output(`[${itr.startTime},${track+4}`)
-
-				if (itr.duration === undefined) {
-					append_to_output(`,0]`)
-				} else {
-					append_to_output(`,${itr.duration}]`)
-				}
-
-				
-				if (i !== beatmap.hitObjects.length - 1) {
-					append_to_output(",")
-				}
-			} else {
-				if (i !== beatmap.hitObjects.length - 1) {
-					append_to_output(",")
-				}
-			}
-
+		if (breaking === false) {
+			append_to_output(",")
+		} else {
+			break;
 		}
-		
-		append_to_output(`]}`)
-		append_to_output(`]}}`)
 	}
+
+	append_to_output(`]}}`)
 
 	if (options.format === true) {
 		console.log(rtv_lua)
